a9af38d1b933ed205b3e1e2b7c0ac8b29cf2d268
rate <rate98326@gmail.com>
2020-04-16 23:28:15 +0900

    最適最低機会判断のスタブ作成。微妙な高速化対応

--- BinanceArbitrage.py
+++ BinanceArbitrage.py
@@ -17,7 +17,8 @@ api_secret = config.get('api', 'secret')
 commission = 0.00075 * 3
 
 # 利益率設定
-minROI = 1.0003
+#minROI = 1.0003
+minROI = 0.997
 minROI += commission
 
 # 全コイン種読み込み
@@ -49,156 +50,167 @@ def test_time():
 def update_orderbook_dict(msg):
     for d in msg:
         orderbook_tickers_dict[d['s']] = d
-    data = ArbitrageCheck()
-    if data.count != 0:
-        print(data)
+    data = arbitrageCheck()
+    transaction = getBestTransaction(data)
+    print(transaction)
 
 # callback function for start_user_socket
 def update_user(msg):
     pass
 
+# 全シンボルから有効なシンボルの組み合わせを返す
 def validData(tgt, piv, alt):
-    validData = set()
+    validData = []
     if tgt+piv in symbols:
-        validData.add(tgt+piv)
+        validData.append(tgt+piv)
     elif piv+tgt in symbols:
-        validData.add(piv+tgt)
+        validData.append(piv+tgt)
 
     if piv+alt in symbols:
-        validData.add(piv+alt)
+        validData.append(piv+alt)
     elif alt+piv in symbols:
-        validData.add(alt+piv)
+        validData.append(alt+piv)
 
     if tgt+alt in symbols:
-        validData.add(tgt+alt)
+        validData.append(tgt+alt)
     elif alt+tgt in symbols:
-        validData.add(alt+tgt)
+        validData.append(alt+tgt)
 
     return validData
 
 # 3通貨間アービトラージが可能か確認
-def ArbitrageCheck():
+def arbitrageCheck():
     tgt = target
     data = []
-    start = time.time()
+    #start = time.time()
     for piv in pivots:
         for alt in alts:
             vd = validData(tgt, piv, alt)
             if len(vd) < 3:
                 continue
             try:
-                if vd[0].find(tgt) == 0:
-                    tgtpiv_b = float(orderbook_tickers_dict[vd[0]]['b'])
-                    if vd[1].find(piv) == 0:
-                        pivalt_b = float(orderbook_tickers_dict[vd[1]]['b'])
-                        if vd[2].find(alt) == 0:
-                            alttgt_b = float(orderbook_tickers_dict[vd[2]]['b'])
+                vd0 = vd[0]
+                vd1 = vd[1]
+                vd2 = vd[2]
+                if vd0.find(tgt) == 0:
+                    tgtpiv_b = float(orderbook_tickers_dict[vd0]['b'])
+                    if vd1.find(piv) == 0:
+                        pivalt_b = float(orderbook_tickers_dict[vd1]['b'])
+                        if vd2.find(alt) == 0:
+                            alttgt_b = float(orderbook_tickers_dict[vd2]['b'])
                             roi = tgtpiv_b * pivalt_b * alttgt_b
                             if roi > minROI:
                                 data.append({
                                         'serial':1,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], tgtpiv_b, 'sell'],
-                                        '2nd':[vd[1], pivalt_b, 'sell'],
-                                        '3rd':[vd[2], alttgt_b, 'sell']
+                                        '1st':[vd0, tgtpiv_b, 'sell'],
+                                        '2nd':[vd1, pivalt_b, 'sell'],
+                                        '3rd':[vd2, alttgt_b, 'sell']
                                     })
                         else:
-                            tgtalt_a = float(orderbook_tickers_dict[vd[2]]['a'])
+                            tgtalt_a = float(orderbook_tickers_dict[vd2]['a'])
                             roi = (tgtpiv_b * pivalt_b) / tgtalt_a
                             if roi > minROI:
                                 data.append({
                                         'serial':2,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], tgtpiv_b, 'sell'],
-                                        '2nd':[vd[1], pivalt_b, 'sell'],
-                                        '3rd':[vd[2], tgtalt_a, 'buy']
+                                        '1st':[vd0, tgtpiv_b, 'sell'],
+                                        '2nd':[vd1, pivalt_b, 'sell'],
+                                        '3rd':[vd2, tgtalt_a, 'buy']
                                     })
                     else:
-                        altpiv_a = float(orderbook_tickers_dict[vd[1]]['a'])
-                        if vd[2].find(alt) == 0:
-                            alttgt_b = float(orderbook_tickers_dict[vd[2]]['b'])
+                        altpiv_a = float(orderbook_tickers_dict[vd1]['a'])
+                        if vd2.find(alt) == 0:
+                            alttgt_b = float(orderbook_tickers_dict[vd2]['b'])
                             roi = (tgtpiv_b / altpiv_a) * alttgt_b
                             if roi > minROI:
                                 data.append({
                                         'serial':3,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], tgtpiv_b, 'sell'],
-                                        '2nd':[vd[1], altpiv_a, 'buy'],
-                                        '3rd':[vd[2], alttgt_b, 'sell']
+                                        '1st':[vd0, tgtpiv_b, 'sell'],
+                                        '2nd':[vd1, altpiv_a, 'buy'],
+                                        '3rd':[vd2, alttgt_b, 'sell']
                                     })
                         else:
-                            tgtalt_a = float(orderbook_tickers_dict[vd[2]]['a'])
+                            tgtalt_a = float(orderbook_tickers_dict[vd2]['a'])
                             roi = (tgtpiv_b / altpiv_a) / tgtalt_a
                             if roi > minROI:
                                 data.append({
                                         'serial':4,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], tgtpiv_b, 'sell'],
-                                        '2nd':[vd[1], altpiv_a, 'buy'],
-                                        '3rd':[vd[2], tgtalt_a, 'buy']
+                                        '1st':[vd0, tgtpiv_b, 'sell'],
+                                        '2nd':[vd1, altpiv_a, 'buy'],
+                                        '3rd':[vd2, tgtalt_a, 'buy']
                                     })
                 else:
-                    pivtgt_a = float(orderbook_tickers_dict[vd[0]]['a'])
-                    if vd[1].find(piv) == 0:
-                        pivalt_b = float(orderbook_tickers_dict[vd[1]]['b'])
-                        if vd[2].find(alt) == 0:
-                            alttgt_b = float(orderbook_tickers_dict[vd[2]]['b'])
+                    pivtgt_a = float(orderbook_tickers_dict[vd0]['a'])
+                    if vd1.find(piv) == 0:
+                        pivalt_b = float(orderbook_tickers_dict[vd1]['b'])
+                        if vd2.find(alt) == 0:
+                            alttgt_b = float(orderbook_tickers_dict[vd2]['b'])
                             roi = 1 / ((pivtgt_a / pivalt_b) / alttgt_b)
                             if roi > minROI:
                                 data.append({
                                         'serial':5,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], pivtgt_a, 'buy'],
-                                        '2nd':[vd[1], pivalt_b, 'sell'],
-                                        '3rd':[vd[2], alttgt_b, 'sell']
+                                        '1st':[vd0, pivtgt_a, 'buy'],
+                                        '2nd':[vd1, pivalt_b, 'sell'],
+                                        '3rd':[vd2, alttgt_b, 'sell']
                                     })
                         else:
-                            tgtalt_a = float(orderbook_tickers_dict[vd[2]]['a'])
+                            tgtalt_a = float(orderbook_tickers_dict[vd2]['a'])
                             roi = 1 / ((pivtgt_a / pivalt_b) * tgtalt_a)
                             if roi > minROI:
                                 data.append({
                                         'serial':6,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], pivtgt_a, 'buy'],
-                                        '2nd':[vd[1], pivalt_b, 'sell'],
-                                        '3rd':[vd[2], tgtalt_a, 'buy']
+                                        '1st':[vd0, pivtgt_a, 'buy'],
+                                        '2nd':[vd1, pivalt_b, 'sell'],
+                                        '3rd':[vd2, tgtalt_a, 'buy']
                                     })
                     else:
-                        altpiv_a = float(orderbook_tickers_dict[vd[1]]['a'])
-                        if vd[2].find(alt) == 0:
-                            alttgt_b = float(orderbook_tickers_dict[vd[2]]['b'])
+                        altpiv_a = float(orderbook_tickers_dict[vd1]['a'])
+                        if vd2.find(alt) == 0:
+                            alttgt_b = float(orderbook_tickers_dict[vd2]['b'])
                             roi = 1 / ((pivtgt_a * altpiv_a) / alttgt_b)
                             if roi > minROI:
                                 data.append({
                                         'serial':7,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], pivtgt_a, 'buy'],
-                                        '2nd':[vd[1], altpiv_a, 'buy'],
-                                        '3rd':[vd[2], alttgt_b, 'sell']
+                                        '1st':[vd0, pivtgt_a, 'buy'],
+                                        '2nd':[vd1, altpiv_a, 'buy'],
+                                        '3rd':[vd2, alttgt_b, 'sell']
                                     })
                         else:
-                            tgtalt_a = float(orderbook_tickers_dict[vd[2]]['a'])
+                            tgtalt_a = float(orderbook_tickers_dict[vd2]['a'])
                             roi = 1 / (pivtgt_a * altpiv_a * tgtalt_a)
                             if roi > minROI:
                                 data.append({
                                         'serial':8,
                                         'roi':roi,
                                         'target':tgt,
-                                        '1st':[vd[0], pivtgt_a, 'buy'],
-                                        '2nd':[vd[1], altpiv_a, 'buy'],
-                                        '3rd':[vd[2], tgtalt_a, 'buy']
+                                        '1st':[vd0, pivtgt_a, 'buy'],
+                                        '2nd':[vd1, altpiv_a, 'buy'],
+                                        '3rd':[vd2, tgtalt_a, 'buy']
                                     })
             except:
                 continue
-    print('elapsed_time:{}'.format(time.time() - start) + '[sec]')
+    #print('elapsed_time:{}'.format(time.time() - start) + '[sec]')
     return data
+
+# 利益が出る裁定機会の中から最適な取引を返す
+def getBestTransaction(data):
+    #TODO ROIが最も高い
+    #TODO 量が可能
+    sortedData = sorted(data, key=lambda x:x['roi'], reverse=True)
+    print(sortedData)
 
 bm = BinanceSocketManager(client)
 bm.start_ticker_socket(update_orderbook_dict)
